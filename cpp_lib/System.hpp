8d91ab2cbb53a19c7cc7a97b0980b01c8ad255a8
Roman Romanovskiy <romanovskiy_roma_2004@mail.ru>
2025-03-13 15:43:07 +0300

    -

---
+++ cpp_lib/System.hpp
@@ -0,0 +1,29 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __STM32F30x_SYSTEM_HPP
+#define __STM32F30x_SYSTEM_HPP
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f30x.h"
+#include "stm32f30x_rcc.h"
+#include "stm32f30x_syscfg.h"
+
+/* Defines  ------------------------------------------------------------------*/
+
+class System
+{
+private:
+    RCC_ClocksTypeDef RCC_Clocks; // structure used for setting up the SysTick Interrupt
+
+public:
+    System()
+    {
+        __disable_irq();
+        __enable_irq();
+        
+        RCC_GetClocksFreq(&RCC_Clocks);
+        if (SysTick_Config(RCC_Clocks.HCLK_Frequency / 1000))
+            while (1); // will end up in this infinite loop if there was an error with Systick_Config
+    }
+};
+
+#endif /*   __STM32F30x_SYSTEM_HPP   */
